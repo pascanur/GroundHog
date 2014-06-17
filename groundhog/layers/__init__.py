e1edf6eaece65644c627b053c5df1b0a3868024c
rizar <dimabv@tut.by>
2014-06-17 13:45:24 -0400

    Export Layer

--- groundhog/layers/__init__.py
+++ groundhog/layers/__init__.py
@@ -12,4 +12,4 @@ from ff_layers import maxpool, maxpool_ntimes, minpool, minpool_ntimes, \
         tanh, sigmoid, rectifier, hard_sigmoid, hard_tanh
 
 from cost_layers import SoftmaxLayer, SigmoidLayer
-from basic import Operator
+from basic import Layer, Operator
