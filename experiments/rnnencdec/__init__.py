a6e0951042ba7432c381e52d534962da1f785f79
rizar <dimabv@tut.by>
2014-07-08 11:03:50 -0400

    Refactor data processing a bit

--- experiments/rnnencdec/__init__.py
+++ experiments/rnnencdec/__init__.py
@@ -1,6 +1,7 @@
 from encdec import RNNEncoderDecoder
 from encdec import get_batch_iterator
 from encdec import parse_input
+from encdec import create_padded_batch
 
 from state import prototype_state
 from state import prototype_sentence_state
