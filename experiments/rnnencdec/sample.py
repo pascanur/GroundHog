ded8f631f67ba34bf7f387c0655cd949cf5eba36
rizar <dimabv@tut.by>
2014-07-17 17:00:24 -0400

    Speed up beam search + time it

--- experiments/rnnencdec/sample.py
+++ experiments/rnnencdec/sample.py
@@ -4,6 +4,7 @@ import argparse
 import cPickle
 import traceback
 import logging
+import time
 
 import numpy
 
@@ -11,6 +12,17 @@ import experiments.rnnencdec
 from experiments.rnnencdec import RNNEncoderDecoder, parse_input
 
 logger = logging.getLogger(__name__)
+
+class Timer(object):
+
+    def __init__(self):
+        self.total = 0
+
+    def start(self):
+        self.start_time = time.time()
+
+    def finish(self):
+        self.total += time.time() - self.start_time
 
 class BeamSearch(object):
 
@@ -27,6 +39,14 @@ class BeamSearch(object):
         self.comp_next_states = self.enc_dec.create_next_states_computer()
 
     def search(self, seq, n_samples, ignore_unk=False, minlen=1):
+        total_timer = Timer()
+        next_probs_timer = Timer()
+        next_states_timer = Timer()
+        new_trans_timer = Timer()
+        choice_timer = Timer()
+
+        total_timer.start()
+
         c = self.comp_repr(seq)[0]
         states = map(lambda x : x[None, :], self.comp_init_states(c))
 
@@ -45,40 +65,49 @@ class BeamSearch(object):
             last_words = (numpy.array(map(lambda t : t[-1], trans))
                     if k > 0
                     else numpy.zeros(beam_size, dtype="int64"))
-            probs = self.comp_next_probs(c, last_words, *states)[0]
+            next_probs_timer.start()
+            log_probs = numpy.log(self.comp_next_probs(c, last_words, *states)[0])
+            next_probs_timer.finish()
+
+            # Adjust log probs according to search restrictions
             if ignore_unk:
-                probs[:,self.unk_id] = -numpy.inf
+                log_probs[:,self.unk_id] = -numpy.inf
             if k < minlen:
-                probs[:,self.eos_id] = -numpy.inf
+                log_probs[:,self.eos_id] = -numpy.inf
 
-            n_choices = beam_size * n_samples
-            choices = numpy.zeros((n_choices, 2), dtype="int64")
-            choice_costs = numpy.zeros(n_choices)
+            choice_timer.start()
+            # Find the best options by calling argpartition of flatten array
+            next_costs = numpy.array(costs)[:, None] - log_probs
+            flat_next_costs = next_costs.flatten()
+            best_costs_indices = numpy.argpartition(
+                    flat_next_costs.flatten(),
+                    n_samples)[:n_samples]
 
-            for i in range(beam_size):
-                best_words = numpy.argsort(probs[i])[-n_samples:]
-                rrange = slice(n_samples * i, n_samples * (i + 1))
-                choices[rrange, 0] = i
-                choices[rrange, 1] = best_words
-                choice_costs[rrange] =\
-                        costs[i] - numpy.log(probs[i, best_words])
+            # Decypher flatten indices
+            voc_size = log_probs.shape[1]
+            trans_indices = best_costs_indices / voc_size
+            word_indices = best_costs_indices % voc_size
+            costs = flat_next_costs[best_costs_indices]
+            choice_timer.finish()
 
-            best_choices = numpy.argsort(choice_costs)[:n_samples]
-
+            new_trans_timer.start()
             new_trans = [[]] * n_samples
             new_costs = numpy.zeros(n_samples)
             new_states = [numpy.zeros((n_samples, c.shape[0]), dtype="float32") for level
                     in range(num_levels)]
             inputs = numpy.zeros(n_samples, dtype="int64")
-            for i, j in enumerate(best_choices):
-                orig_idx = choices[j, 0]
-                next_word = choices[j, 1]
+            for i, (orig_idx, next_word, next_cost) in enumerate(
+                    zip(trans_indices, word_indices, costs)):
                 new_trans[i] = trans[orig_idx] + [next_word]
-                new_costs[i] = choice_costs[j]
+                new_costs[i] = next_cost
                 for level in range(num_levels):
                     new_states[level][i] = states[level][orig_idx]
                 inputs[i] = next_word
+            new_trans_timer.finish()
+
+            next_states_timer.start()
             new_states = self.comp_next_states(c, inputs, *new_states)
+            next_states_timer.finish()
 
             trans = []
             costs = []
@@ -94,6 +123,15 @@ class BeamSearch(object):
                     fin_costs.append(new_costs[i])
             states = map(lambda x : x[indices], new_states)
 
+        total_timer.finish()
+        logging.debug("""Took {}, next probs took {},
+                next states took {},
+                new translations took {}, choices took {}""".format(
+            total_timer.total,
+            next_probs_timer.total,
+            next_states_timer.total,
+            new_trans_timer.total,
+            choice_timer.total))
         return fin_trans, fin_costs
 
 def indices_to_words(i2w, seq):
