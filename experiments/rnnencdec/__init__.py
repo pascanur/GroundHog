ba745d03c11d7478c4da5a68246ec9d461077365
rizar <dimabv@tut.by>
2014-08-29 11:45:22 -0400

    Remove states that do not exist anymore

--- experiments/rnnencdec/__init__.py
+++ experiments/rnnencdec/__init__.py
@@ -6,6 +6,3 @@ from encdec import create_padded_batch
 from state import prototype_state
 from state import prototype_search_state
 from state import prototype_sentence_state
-from state import prototype_autoenc_state
-from state import prototype_phrase_en_zn_state
-from state import prototype_de2en_state
