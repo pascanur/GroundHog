de57a95b37416f3e17c61ad566b30333aac1a545
rizar <dimabv@tut.by>
2014-09-03 20:19:17 -0400

    Commit BLEU stuff to analysis

--- experiments/rnnencdec/analysis.py
+++ experiments/rnnencdec/analysis.py
@@ -1,6 +1,7 @@
 import logging
 import numpy
 import pandas
+from collections import Counter
 
 logger = logging.getLogger(__name__)
 
@@ -31,3 +32,27 @@ def load_n_show_timings(axes, timings, legend, **kwargs):
     show_args = set(kwargs.keys()).intersection(['y', 'hours'])
     show_timings(axes, datas, legend,
             **{k : kwargs[k] for k in show_args})
+
+def bleu_stats(hypothesis, reference):
+    yield len(hypothesis)
+    yield len(reference)
+    for n in xrange(1, 5):
+        s_ngrams = Counter([tuple(hypothesis[i:i + n]) for i in xrange(len(hypothesis) + 1 - n)])
+        r_ngrams = Counter([tuple(reference[i:i + n]) for i in xrange(len(reference) + 1 - n)])
+        yield sum((s_ngrams & r_ngrams).values())
+        yield max(len(hypothesis) + 1 - n, 0)
+
+def bleu(stats):
+    stats = numpy.atleast_2d(numpy.asarray(stats))[:, :10].sum(axis=0)
+    if not all(stats):
+        return 0
+    c, r = stats[:2]
+    log_bleu_prec = sum([numpy.log(float(x) / y) for x, y in zip(stats[2::2], stats[3::2])]) / 4.
+    return numpy.exp(min(0, 1 - float(r) / c) + log_bleu_prec) * 100
+
+def smoothed_bleu(stats):
+    c, r = stats[:2]
+    log_bleu_prec = sum([numpy.log((1 + float(x)) / (1 + y)) for x, y in zip(stats[2::2], stats[3::2])]) / 4.
+    return numpy.exp(min(0, 1 - float(r) / c) + log_bleu_prec) * 100
+
+
