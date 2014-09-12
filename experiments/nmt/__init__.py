7ff25595540397eda5a05e8c6a2af40483880bc3
rizar <dimabv@tut.by>
2014-09-12 09:18:13 -0400

    Finish the last commit

--- experiments/nmt/__init__.py
+++ experiments/nmt/__init__.py
@@ -4,6 +4,7 @@ from encdec import parse_input
 from encdec import create_padded_batch
 
 from state import\
+    prototype_state,\
     prototype_phrase_state,\
     prototype_encdec_state,\
     prototype_search_state
